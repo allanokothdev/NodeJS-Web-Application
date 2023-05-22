310d4de8a24f9714430eeaf6f90dcbe2de339614
Allan Okoth <allanokoth695@gmail.com>
2023-05-23 00:24:01 +0200

    file read/write operations

--- readfils.js
+++ readfils.js
@@ -1,4 +1,18 @@
 const fs = require("fs");
 
+//  Read asynchronously operation
 const readMeFile = fs.readFileSync("./read.txt", "utf-8");
 console.log(readMeFile);  // File contents
+
+//Write asynchronously operation
+fs.writeFileSync("./read-new.txt", readMeFile)
+
+//  synchronously
+fs.readFile("./read.txt", "utf-8", (err, data) => {
+    if(err) {
+        console.error(err);
+    }
+    console.log(data)
+});
+
+console.log("Am I First")
