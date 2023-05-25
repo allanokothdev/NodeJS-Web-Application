ec26d085ba4dad448e6e14fda1914363689dc0fb
Allan Okoth <allanokoth695@gmail.com>
2023-05-26 01:34:53 +0200

    build(deps): update app.js, package-lock.json and package.json

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
-let USER_DATA = require("./users.json");
+const mongoose = require("mongoose");
+const app = express();
 
-app.set("view engine", "ejs");  // setting up template engine to ejs
+// This is only require on local machine
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+const PORT = process.env.PORT || 3000;
+
+// connect to the database
+async function connectDB() {
+    const { MONGO_USER, MONGO_HOST, MONGO_PASS, MONGO_DB } = process.env;
+    await mongoose.connect(
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        }
+    );
+}
+
+connectDB();
+
+// Create Schema for users
+const usersSchema = new mongoose.Schema({
+    age: Number,
+    name: String,
+    email: String,
+});
+
+// Create Modal for users
+const UsersModal = new mongoose.model("users", usersSchema);
+
+app.set("view engine", "ejs");
 
 app.use((req, res, next) => {
     res.append("Access-Control-Allow-Origin", ["*"]);
@@ -18,34 +50,37 @@ app.use((req, res, next) => {
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-function makeId(length) {
-    var result = "";
-    var characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
+app.use("/assets", express.static("assets"));
 
 const getUserInfo = (id) => {
     return USER_DATA.filter((obj) => obj._id === id);
 };
 
+// Get list of users, and set data
 app.get("/", (req, res) => {
-    res.render("index", {
-        data: USER_DATA,
+    UsersModal.find({}, function (err, docs) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.render("index", {
+            data: docs,
+        });
     });
 });
 
+// Get single user by id, and set data
 app.get("/user/:id", (req, res) => {
-    res.render("user", {
-        data: getUserInfo(req.params.id)[0] || {},
+    UsersModal.findOne({ _id: req.params.id }, function (err, doc) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.render("user", {
+            data: doc,
+        });
     });
 });
 
+// Loading view of contact page
 app.get("/contact", (req, res) => {
     res.sendFile(`${__dirname}/contact.html`);
 });
@@ -54,37 +89,74 @@ app.get("/contact", (req, res) => {
 
 // get all users
 app.get("/api/users", (req, res) => {
-    res.send(USER_DATA);
+    UsersModal.find({}, function (err, docs) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.send(docs);
+    });
 });
 
 // get single user by id
 app.get("/api/user/:id", (req, res) => {
-    res.send(getUserInfo(req.params.id));
+    UsersModal.findOne({ _id: req.params.id }, function (err, doc) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.send(doc);
+    });
 });
 
 // delete single user by id
 app.delete("/api/user/:id", (req, res) => {
-    const index = USER_DATA.findIndex((obj) => obj._id === req.params.id);
-    res.send(USER_DATA.splice(index, 1));
+    UsersModal.findOneAndDelete({ _id: req.params.id }, function (err, doc) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.send(doc);
+    });
+});
+
+// update single user by id
+app.put("/api/user/:id", (req, res) => {
+    const { name, age, email } = req.body;
+    if (req.params.id) {
+        UsersModal.updateOne(
+            { _id: req.params.id },
+            { name, age, email },
+            function (err, doc) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.send(doc);
+            }
+        );
+    } else {
+        res.status(500).send("id is missing in params");
+    }
 });
 
 // add new user
 app.post("/api/user", (req, res) => {
     const { name, age, email } = req.body;
+
     if (name && age && email) {
-        USER_DATA = [
-            ...USER_DATA,
+        UsersModal.create(
             {
-                _id: makeId(24),
                 age,
                 name,
                 email,
             },
-        ];
-        res.send(USER_DATA);
+            function (err, doc) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.send(doc);
+            }
+        );
     } else {
         res.status(500).send("error, user not added.");
     }
 });
 
-app.listen(3000);
+app.listen(PORT);
